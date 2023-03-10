dbf9b02ce9146a383627e2e3a186bd580a3516f6
Kolya Movchan <kmovchan261@gmail.com>
2023-03-10 20:47:18 +0200

    clone repository

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-# React TypeScript Starter Pack
+## Introduction
+The React todo app that I built is a fully functional application that allows users to add, delete, mark todos as completed or that need to be done, and update data on the API. The app was built using a variety of tools and methods that showcase my proficiency in React, TypeScript, and API integration.
 
-To use this template click `Use this template`
+## React components
+I created custom React components that were used throughout the app, allowing me to create a more modular and flexible architecture. These components were designed to be easily reusable and allowed me to create a consistent user interface throughout the app.
 
-### Available Scripts
+## React Events
+I used React.FormEvent and React.ChangeEvent to manage form and input events, allowing users to add new todos and update existing ones.
 
-`Deploy` - available to deploy your application to gh-pages
+I also utilized the React Keyboard Event to add keyboard shortcuts to the app. This allowed users to perform various actions by simply pressing a key on their keyboard, without having to navigate through menus or click buttons. For example, I used the keydown event to allow users to mark a todo as completed by pressing the Enter key.
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+## React Router
+I used React Router with Routes to manage the app's navigation and allow users to move between different pages of the app. This allowed me to create a more seamless user experience that felt like a natural part of the application.
+
+## Async function
+The async function was used to handle asynchronous requests, and I also used the filter and map methods to manipulate arrays of data. This allowed me to filter and sort todos based on their status and make updates to the data as necessary.
+
+## Classnames library
+The classnames library was used extensively throughout the code to dynamically apply CSS classes to different elements based on their state. This allowed me to create a more visually engaging app that provided clear feedback to users on the status of their todos.
+
+## API
+API integration was a key part of the project, and I used promises such as then, catch, and finally to handle HTTP requests and responses. This allowed me to manage data on the server and ensure that the app remained in sync with the API at all times.
+
+## Hooks
+The useState hook was used to manage the state of the app, and I also used useEffect with dependencies to control the current todos. This allowed me to update the app's state and re-render components when necessary, ensuring that the app remained responsive and up-to-date at all times. 
+
+I also utilized the useContext hook to manage global state in the app. This allowed me to pass data between components without having to rely on prop drilling or other workarounds. The use of useContext made it much easier to manage state in the app and allowed me to create a more efficient and streamlined development process.
+
+## CSSTransition group
+I also used the CSSTransition group to animate the appearance and disappearance of todos. This allowed me to create a more visually engaging user experience that provided clear feedback on the status of the user's todos. The CSSTransition group made it easy to add animations to the app without having to write complex CSS code or use external libraries.
+
+## TypeScript
+Finally, I paid a lot of attention to TypeScript and used interfaces for todos and users, the pick method, and enums for storing error text. This helped me to catch errors early in the development process and ensure that the app remained stable and reliable.
+
+## Summary
+In summary, the React todo app that I built showcases my proficiency in a wide range of tools and methods, including React Router, classnames, API integration, promises, hooks, async functions, array methods, form events, logical operators, components, and TypeScript. I believe that the app demonstrates my ability to build complex, fully functional applications that are stable, reliable, and easy to use.
+
+Why do you think the React developers quit their jobs? Because they didn't get setState of the company's goals. And I hope that in this project I did a great job with setState.
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,262 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import classNames from 'classnames';
 
-interface Props {
-  onClick: () => void;
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import {
+  getTodos,
+  postTodos,
+  deleteTodos,
+  patchTodos,
+} from './api/todos';
+
+import { Filter, Filters } from './components/React/Filter';
+import { AuthContext } from './components/Auth/AuthContext';
+import { NewTodo } from './components/React/NewTodo';
+import { TodoList } from './components/React/TodoList';
+import { Todo, TodoUpdateData } from './types/Todo';
+
+enum Errors {
+  Server = 'Unable to load data from the Server',
+  Post = 'Unable to add the Todo',
+  Add = 'Title can\'t be empty',
+  Delete = 'Unable to delete a todo',
+  Update = 'Unable to update a todo',
 }
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
 export const App: React.FC = () => {
+  const user = useContext(AuthContext);
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filters>(Filters.All);
+  const [error, setError] = useState('');
+  const [input, setInput] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [todoOnload, setTodoOnLoad] = useState<Todo | null>(null);
+  const [todoIdsOnLoad, setTodoIdsOnLoad] = useState<number[]>([]);
+  const [isActiveToggle, setIsActiveToggle] = useState(false);
+
+  const putTodoOnLoad = (todoId: number) => {
+    setTodoIdsOnLoad([...todoIdsOnLoad, todoId]);
+  };
+
+  const putAllTodosOnLoad = (selectedTodos: Todo[]) => {
+    const selectedTodoIds = selectedTodos.map(todo => todo.id);
+
+    setTodoIdsOnLoad(selectedTodoIds);
+  };
+
+  const fetch = () => {
+    if (user) {
+      getTodos(user.id)
+        .then(todosFromServer => setTodos(todosFromServer))
+        .catch(() => setError(Errors.Server))
+        .finally(() => setTodoIdsOnLoad([]));
+    }
+  };
+
+  const deleteData = (id: number) => {
+    deleteTodos(id)
+      .then(() => fetch())
+      .catch(() => setError(Errors.Delete));
+  };
+
+  const patch = async (
+    id: number,
+    data: TodoUpdateData,
+    toggleMode = false,
+  ) => {
+    putTodoOnLoad(id);
+
+    try {
+      await patchTodos(id, data);
+
+      setTodos((prevTodos) => {
+        return (
+          prevTodos.map(todo => {
+            if (toggleMode) {
+              return { ...todo, ...data };
+            }
+
+            if (todo.id === id) {
+              return { ...todo, ...data };
+            }
+
+            return todo;
+          })
+        );
+      });
+    } catch {
+      setError(Errors.Update);
+    } finally {
+      setTodoIdsOnLoad((prevTodoIds) => (
+        prevTodoIds.filter(todoId => todoId !== id)
+      ));
+    }
+  };
+
+  const handleCompletedCheckBox = async (todo: Todo) => {
+    patch(todo.id, { completed: !todo.completed });
+  };
+
+  const handleTodoDeleteButton = (id: number) => {
+    deleteData(id);
+    putTodoOnLoad(id);
+  };
+
+  const handleClearButton = () => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    putAllTodosOnLoad(completedTodos);
+
+    completedTodos.map(todo => deleteData(todo.id));
+  };
+
+  const handleToggleAll = () => {
+    setIsActiveToggle(!isActiveToggle);
+
+    todos.forEach(todo => patch(todo.id, { completed: !isActiveToggle }, true));
+
+    putAllTodosOnLoad(todos);
+  };
+
+  const handleErrorCloser = () => setError('');
+
+  const handleFormSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!input) {
+      setError(Errors.Add);
+      setTimeout(() => setError(''), 3000);
+
+      return;
+    }
+
+    if (user) {
+      const newTodoTemplate: Todo = {
+        id: 0,
+        userId: user.id,
+        title: input,
+        completed: false,
+      };
+
+      setIsAdding(true);
+
+      try {
+        setTodoOnLoad(newTodoTemplate);
+
+        const newTodo = await postTodos(newTodoTemplate);
+
+        setTodos([...todos, newTodo]);
+      } catch {
+        setError(Errors.Post);
+      } finally {
+        setInput('');
+        setIsAdding(false);
+        setTodoOnLoad(null);
+      }
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.currentTarget.value);
+    setError('');
+  };
+
+  const handleExtraInputChange = (id: number, title: string) => {
+    if (!title) {
+      putTodoOnLoad(id);
+      deleteData(id);
+
+      return;
+    }
+
+    patch(id, { title });
+  };
+
+  const handleTodosFilter = (filterType: Filters) => setFilter(filterType);
+
+  useEffect(() => fetch(), []);
+
+  useEffect(() => {
+    const allCompleted = todos.every(todo => (todo.completed));
+
+    if (allCompleted) {
+      setIsActiveToggle(true);
+    } else {
+      setIsActiveToggle(false);
+    }
+  }, [todos]);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <header className="todoapp__header">
+          {!!todos.length && (
+            <button
+              data-cy="ToggleAllButton"
+              type="button"
+              className={classNames(
+                'todoapp__toggle-all', { active: isActiveToggle },
+              )}
+              onClick={() => handleToggleAll()}
+            />
+          )}
+
+          <NewTodo
+            input={input}
+            isAdding={isAdding}
+            todoOnload={todoOnload}
+            onInputChange={handleInputChange}
+            onSubmit={handleFormSubmit}
+          />
+        </header>
+
+        {!!todos.length && (
+          <>
+            <TodoList
+              todos={todos}
+              filter={filter}
+              todoOnLoad={todoOnload}
+              todoIdsOnLoad={todoIdsOnLoad}
+              onTodoDelete={handleTodoDeleteButton}
+              onTodoComplete={handleCompletedCheckBox}
+              saveInputChange={handleExtraInputChange}
+            />
+
+            <Filter
+              filter={filter}
+              todos={todos}
+              onFilterChange={handleTodosFilter}
+              onCompletedClear={handleClearButton}
+            />
+          </>
+        )}
+      </div>
+
+      <div
+        data-cy="ErrorNotification"
+        className={classNames(
+          'notification is-danger is-light has-text-weight-normal',
+          {
+            hidden: !error,
+          },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={handleErrorCloser}
+        />
+        {error}
+      </div>
     </div>
   );
 };
